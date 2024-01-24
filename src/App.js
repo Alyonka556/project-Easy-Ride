25e0870c9f67e4155709e8bb7b2405b3968105da
Alyonka556 <142902721+Alyonka556@users.noreply.github.com>
2024-01-24 14:54:56 +0100

    add_Lazy_Suspense

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import "./index.css";
-import Home from "./pages/Home";
-import Catalog from "./pages/Catalog";
-import Favorites from "./pages/Favorites";
-import NotFound from "./pages/NotFound";
-import Layout from "./components/Layout/Layout";
+import React, { Suspense, lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './index.css';
+import NotFound from './pages/NotFound';
+import Layout from './components/Layout/Layout';
+
+const Home = lazy(() => import('./pages/Home'));
+const Catalog = lazy(() => import('./pages/Catalog'));
+const Favorites = lazy(() => import('./pages/Favorites'));
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/catalog" element={<Catalog />} />
-        <Route path="/favorites" element={<Favorites />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
